3da8600a022c23c9fd76b0320ebde36156c42918
Lalit Sirsikar <lalit@datatron.io>
2020-03-19 23:54:25 -0700

    Upgrade to debian:stretch and other relevant upgrades.

--- Dockerfile
+++ Dockerfile
@@ -1,36 +1,25 @@
-FROM debian:7.8
+# As of March 18, 2020 - corona virus scare time
+
+# https://www.debian.org/releases/
+FROM debian:stretch
+
 MAINTAINER Mike Babineau michael.babineau@gmail.com
 
-ENV \
-    ZK_RELEASE="http://www.apache.org/dist/zookeeper/zookeeper-3.4.6/zookeeper-3.4.6.tar.gz" \
-    EXHIBITOR_POM="https://raw.githubusercontent.com/Netflix/exhibitor/d911a16d704bbe790d84bbacc655ef050c1f5806/exhibitor-standalone/src/main/resources/buildscripts/standalone/maven/pom.xml" \
-    # Append "+" to ensure the package doesn't get purged
-    BUILD_DEPS="curl maven openjdk-7-jdk+" \
-    DEBIAN_FRONTEND="noninteractive"
+ENV DEBIAN_FRONTEND="noninteractive"
 
-# Use one step so we can remove intermediate dependencies and minimize size
-RUN \
-    # Install dependencies
-    apt-get update \
-    && apt-get install -y --allow-unauthenticated --no-install-recommends $BUILD_DEPS \
-
-    # Default DNS cache TTL is -1. DNS records, like, change, man.
-    && grep '^networkaddress.cache.ttl=' /etc/java-7-openjdk/security/java.security || echo 'networkaddress.cache.ttl=60' >> /etc/java-7-openjdk/security/java.security \
-
-    # Install ZK
-    && curl -Lo /tmp/zookeeper.tgz $ZK_RELEASE \
+# Ref: https://stackoverflow.com/questions/57031649/how-to-install-openjdk-8-jdk-on-debian-10-buster
+RUN apt-get update -y && apt-get install curl maven wget gnupg software-properties-common openjdk-8-jdk-headless openjdk-8-jdk -y \
+    && curl -Lo /tmp/zookeeper.tar.gz "https://downloads.apache.org/zookeeper/zookeeper-3.4.14/zookeeper-3.4.14.tar.gz" \
     && mkdir -p /opt/zookeeper/transactions /opt/zookeeper/snapshots \
-    && tar -xzf /tmp/zookeeper.tgz -C /opt/zookeeper --strip=1 \
-    && rm /tmp/zookeeper.tgz \
-
+    && tar -xvf /tmp/zookeeper.tar.gz -C /opt/zookeeper --strip=1 \
+    && rm /tmp/zookeeper.tar.gz \
     # Install Exhibitor
     && mkdir -p /opt/exhibitor \
-    && curl -Lo /opt/exhibitor/pom.xml $EXHIBITOR_POM \
+    && curl -Lo /opt/exhibitor/pom.xml "https://raw.githubusercontent.com/Netflix/exhibitor/master/exhibitor-standalone/src/main/resources/buildscripts/standalone/maven/pom.xml" \
     && mvn -f /opt/exhibitor/pom.xml package \
     && ln -s /opt/exhibitor/target/exhibitor*jar /opt/exhibitor/exhibitor.jar \
-
     # Remove build-time dependencies
-    && apt-get purge -y --auto-remove $BUILD_DEPS \
+    && apt-get purge -y --auto-remove curl maven wget gnupg software-properties-common \
     && rm -rf /var/lib/apt/lists/*
 
 # Add the wrapper script to setup configs and exec exhibitor
@@ -40,7 +29,14 @@ ADD include/wrapper.sh /opt/exhibitor/wrapper.sh
 ADD include/web.xml /opt/exhibitor/web.xml
 
 USER root
+
 WORKDIR /opt/exhibitor
+
 EXPOSE 2181 2888 3888 8181
 
 ENTRYPOINT ["bash", "-ex", "/opt/exhibitor/wrapper.sh"]
+
+#ENTRYPOINT ["sleep", "1000000000"]
+
+# TODO - make wrapper.sh run completely and also modify defaults.conf to include backup dir and other variables. Give pr to Dhruv, Zach and Kevin.
+
--- include/wrapper.sh
+++ include/wrapper.sh
@@ -1,95 +1,63 @@
 #! /bin/bash -e
 
 # Generates the default exhibitor config and launches exhibitor
+ZK_DATA_DIR=${ZK_DATA_DIR:-/var/lib/zookeeper/data}
+ZK_LOG_DIR=${ZK_LOG_DIR:-/var/lib/zookeeper/log}
+ZK_ENSEMBLE_SIZE=${ZK_ENSEMBLE_SIZE:-0}
+ZK_SETTLING_PERIOD=${ZK_SETTLING_PERIOD:-120000}
+ZK_FS_CONFIG_DIR=${ZK_FS_CONFIG_DIR:-/var/lib/zookeeper_config}
+ZK_BACKUP_DIR=${ZK_BACKUP_DIR:-/zookeeper/data}
+ZK_BACKUP_CONFIG="--configtype file --fsconfigdir ${ZK_FS_CONFIG_DIR} --filesystembackup true"
+ZK_HOSTNAME=$(hostname)
+ZK_EXHIBITOR_PORT=${ZK_EXHIBITOR_PORT:-8181}
 
-MISSING_VAR_MESSAGE="must be set"
-DEFAULT_AWS_REGION="us-west-2"
-DEFAULT_DATA_DIR="/opt/zookeeper/snapshots"
-DEFAULT_LOG_DIR="/opt/zookeeper/transactions"
-DEFAULT_ZK_ENSEMBLE_SIZE=0
-S3_SECURITY=""
-HTTP_PROXY=""
-: ${HOSTNAME:?$MISSING_VAR_MESSAGE}
-: ${AWS_REGION:=$DEFAULT_AWS_REGION}
-: ${ZK_DATA_DIR:=$DEFAULT_DATA_DIR}
-: ${ZK_LOG_DIR:=$DEFAULT_LOG_DIR}
-: ${ZK_ENSEMBLE_SIZE:=$DEFAULT_ZK_ENSEMBLE_SIZE}
-: ${HTTP_PROXY_HOST:=""}
-: ${HTTP_PROXY_PORT:=""}
-: ${HTTP_PROXY_USERNAME:=""}
-: ${HTTP_PROXY_PASSWORD:=""}
+mkdir -p ${ZK_DATA_DIR} ${ZK_LOG_DIR} ${ZK_FS_CONFIG_DIR}
+
+if [[ -n $ZK_MAX_SERVERS ]] && [[ -n $ZK_STATEFULSET_NAME ]] && [[ -n $ZK_GOVERNING_SERVICE_NAME ]]; then
+  for i in $( eval echo {0..$((ZK_MAX_SERVERS-1))}); do
+    EXHIBITOR_SERVERS_SPEC="${EXHIBITOR_SERVERS_SPEC}S\:$i\:${ZK_STATEFULSET_NAME}-$i.${ZK_GOVERNING_SERVICE_NAME}"
+    if [[ $i -lt $((ZK_MAX_SERVERS-1)) ]]; then
+        EXHIBITOR_SERVERS_SPEC="${EXHIBITOR_SERVERS_SPEC},"
+    fi
+  done
+  ZK_HOSTNAME=${ZK_HOSTNAME}.${ZK_GOVERNING_SERVICE_NAME}
+else
+  EXHIBITOR_SERVERS_SPEC="S\:0\:${ZK_HOSTNAME}"
+fi
 
 cat <<- EOF > /opt/exhibitor/defaults.conf
-	zookeeper-data-directory=$ZK_DATA_DIR
-	zookeeper-install-directory=/opt/zookeeper
-	zookeeper-log-directory=$ZK_LOG_DIR
-	log-index-directory=$ZK_LOG_DIR
-	cleanup-period-ms=300000
-	check-ms=30000
-	backup-period-ms=600000
-	client-port=2181
-	cleanup-max-files=20
-	backup-max-store-ms=21600000
-	connect-port=2888
-	observer-threshold=0
-	election-port=3888
-	zoo-cfg-extra=tickTime\=2000&initLimit\=10&syncLimit\=5&quorumListenOnAllIPs\=true
-	auto-manage-instances-settling-period-ms=0
-	auto-manage-instances=1
-	auto-manage-instances-fixed-ensemble-size=$ZK_ENSEMBLE_SIZE
+auto-manage-instances-fixed-ensemble-size=$ZK_ENSEMBLE_SIZE
+auto-manage-instances-settling-period-ms=$ZK_SETTLING_PERIOD
+auto-manage-instances=1
+backup-max-store-ms=21600000
+backup-period-ms=60000
+backup-extra=directory\=${ZK_BACKUP_DIR}
+check-ms=30000
+cleanup-max-files=20
+cleanup-period-ms=300000
+client-port=2181
+connect-port=2888
+election-port=3888
+log-index-directory=$ZK_LOG_DIR
+observer-threshold=0
+servers-spec=${EXHIBITOR_SERVERS_SPEC}
+zoo-cfg-extra=tickTime\=2000&initLimit\=10&syncLimit\=5&quorumListenOnAllIPs\=true
+zookeeper-data-directory=$ZK_DATA_DIR
+zookeeper-install-directory=/opt/zookeeper
+zookeeper-log-directory=$ZK_LOG_DIR
 EOF
+cat /opt/exhibitor/defaults.conf
 
-
-if [[ -n ${AWS_ACCESS_KEY_ID} ]]; then
-  cat <<- EOF > /opt/exhibitor/credentials.properties
-    com.netflix.exhibitor.s3.access-key-id=${AWS_ACCESS_KEY_ID}
-    com.netflix.exhibitor.s3.access-secret-key=${AWS_SECRET_ACCESS_KEY}
-EOF
-  S3_SECURITY="--s3credentials /opt/exhibitor/credentials.properties"
-fi
-
-if [[ -n ${S3_BUCKET} ]]; then
-  echo "backup-extra=throttle\=&bucket-name\=${S3_BUCKET}&key-prefix\=${S3_PREFIX}&max-retries\=4&retry-sleep-ms\=30000" >> /opt/exhibitor/defaults.conf
-
-  BACKUP_CONFIG="--configtype s3 --s3config ${S3_BUCKET}:${S3_PREFIX} ${S3_SECURITY} --s3region ${AWS_REGION} --s3backup true"
-else
-  BACKUP_CONFIG="--configtype file --fsconfigdir /opt/zookeeper/local_configs --filesystembackup true"
-fi
-
-if [[ -n ${ZK_PASSWORD} ]]; then
-	SECURITY="--security web.xml --realm Zookeeper:realm --remoteauth basic:zk"
-	echo "zk: ${ZK_PASSWORD},zk" > realm
-fi
-
-
-if [[ -n $HTTP_PROXY_HOST ]]; then
-    cat <<- EOF > /opt/exhibitor/proxy.properties
-      com.netflix.exhibitor.s3.proxy-host=${HTTP_PROXY_HOST}
-      com.netflix.exhibitor.s3.proxy-port=${HTTP_PROXY_PORT}
-      com.netflix.exhibitor.s3.proxy-username=${HTTP_PROXY_USERNAME}
-      com.netflix.exhibitor.s3.proxy-password=${HTTP_PROXY_PASSWORD}
-EOF
-
-    HTTP_PROXY="--s3proxy=/opt/exhibitor/proxy.properties"
-fi
-
+[[ -n ${ZK_PASSWORD} ]] && {
+  ZK_SECURITY="--security web.xml --realm Zookeeper:realm --remoteauth basic:zk"
+  echo "zk: ${ZK_PASSWORD},zk" > realm
+}
 
 exec 2>&1
 
-# If we use exec and this is the docker entrypoint, Exhibitor fails to kill the ZK process on restart.
-# If we use /bin/bash as the entrypoint and run wrapper.sh by hand, we do not see this behavior. I suspect
-# some init or PID-related shenanigans, but I'm punting on further troubleshooting for now since dropping
-# the "exec" fixes it.
-#
-# exec java -jar /opt/exhibitor/exhibitor.jar \
-# 	--port 8181 --defaultconfig /opt/exhibitor/defaults.conf \
-# 	--configtype s3 --s3config thefactory-exhibitor:${CLUSTER_ID} \
-# 	--s3credentials /opt/exhibitor/credentials.properties \
-# 	--s3region us-west-2 --s3backup true
-
 java -jar /opt/exhibitor/exhibitor.jar \
-  --port 8181 --defaultconfig /opt/exhibitor/defaults.conf \
-  ${BACKUP_CONFIG} \
-  ${HTTP_PROXY} \
-  --hostname ${HOSTNAME} \
-  ${SECURITY}
+  --port ${ZK_EXHIBITOR_PORT} \
+  --defaultconfig /opt/exhibitor/defaults.conf \
+  --hostname ${ZK_HOSTNAME} \
+  ${ZK_BACKUP_CONFIG} \
+  ${ZK_SECURITY}
